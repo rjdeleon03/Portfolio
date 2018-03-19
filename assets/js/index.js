7d60ab51378c9706de68b353185ae3ef1a35dc0f
Ricardo Pablo <De Leon>
2018-03-19 13:54:20 +0800

    Add simple packery grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,9 @@ $("#a-projects").on("click", function() {
 $("#a-skills").on("click", function() {
     $("html, body").animate({ scrollTop: $("#skills").offset().top }, 1000);
 });
+
+$('.grid').packery({
+    itemSelector: '.grid-item',
+    gutter: '.gutter-sizer',
+    percentPosition: true
+  });
