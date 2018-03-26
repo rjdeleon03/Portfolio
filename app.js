1fdd747b8da878ba53f36b357a14450ff8e374c6
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-26 20:00:38 +0800

    Add details to contact and footer sections

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + "/public"));
 
 // For any path, render index page
 app.get("*", function(req, res) {
-    res.render("index");
+    res.render("index", {year: (new Date()).getFullYear()});
 });
 
 // Listen to router
