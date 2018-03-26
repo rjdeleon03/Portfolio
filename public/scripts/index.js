b821db6305b3fa6dde2e55f84792fbc4dcadcfd9
Ricardo Pablo <De Leon>
2018-03-26 15:34:34 +0800

    Add sticky navbar

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,20 +1,20 @@
-$("#a-top").on("click", function() {
+$(".a-top").on("click", function() {
     $("html, body").animate({ scrollTop: $("#about").offset().top }, 1000);
 });
 
-$("#a-skills").on("click", function() {
+$(".a-skills").on("click", function() {
     $("html, body").animate({ scrollTop: $("#skills").offset().top }, 1000);
 });
 
-$("#a-projects").on("click", function() {
+$(".a-projects").on("click", function() {
     $("html, body").animate({ scrollTop: $("#projects").offset().top }, 1000);
 });
 
-$("#a-contact").on("click", function() {
+$(".a-contact").on("click", function() {
     $("html, body").animate({ scrollTop: $("#contact").offset().top }, 1000);
 });
 
-var $navBarTop = $("#about-nav").offset().top;
+var $navBarTop = $("#skills").offset().top;
 var isPositionFixed = ($("#about-nav").css("position") == "fixed");
 
 // Navbar behavior on scroll
@@ -24,13 +24,13 @@ $(window).scroll(function(e){
 
 // Navbar behavior on resize
 $(window).resize(function(e) {
-    $navBarTop = $("#about-nav").offset().top;
+    $navBarTop = $("#skills").offset().top;
     repositionNavbar();
 });
 
 // Navbar behavior on orientation change
 $(window).on("orientationchange", function(e) {
-    $navBarTop = $("#about-nav").offset().top;
+    $navBarTop = $("#skills").offset().top;
     repositionNavbar();
 });
 
@@ -38,18 +38,17 @@ $(window).on("orientationchange", function(e) {
 function repositionNavbar() {
     var $el = $("#about-nav");     
     if ($(this).scrollTop() >= $navBarTop && !isPositionFixed){ 
-        $("#about-nav-menu").animate({
-            backgroundColor: "rgba(60, 60, 60, 0.95)",
-        }, 300);
+        $("#about-nav-menu").fadeIn(200);
+        $("#about-nav-menu").removeClass("hidden");
         $("#about-nav-menu").addClass("sticky");
         isPositionFixed = true;
     }
     if ($(this).scrollTop() < $navBarTop && $(this).scrollTop() > 0 && isPositionFixed)
     {
-        $("#about-nav-menu").animate({
-            backgroundColor: "rgba(0, 0, 0, 0)",
-        }, 200);
-        $("#about-nav-menu").removeClass("sticky");
-        isPositionFixed = false;
+        $("#about-nav-menu").fadeOut(200, function() {  
+            $("#about-nav-menu").removeClass("sticky");
+            $("#about-nav-menu").addClass("hidden");
+            isPositionFixed = false;
+        });
     } 
 }
